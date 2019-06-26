01572680265f477900e183ba6844463614cb2abf
Jethro Kuan <jethrokuan95@gmail.com>
2019-06-26 19:56:46 +0800

    hide more things

--- init.el
+++ init.el
@@ -20,14 +20,14 @@
 
 (require 'find-lisp)
 
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-
+(tooltip-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
 (blink-cursor-mode -1)
-
 (setq ring-bell-function 'ignore)
-
-(setq inhibit-startup-screen t)
 
 (use-package zenburn-theme
   :init
