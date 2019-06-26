d2c92d586df58b4136aa52373685e73c3d92f728
Jethro Kuan <jethrokuan95@gmail.com>
2019-06-26 19:58:01 +0800

    check

--- init.el
+++ init.el
@@ -20,10 +20,15 @@
 
 (require 'find-lisp)
 
-(tooltip-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'tool-tip-mode)
+  (tool-tip-mode -1))
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
+
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 (blink-cursor-mode -1)
